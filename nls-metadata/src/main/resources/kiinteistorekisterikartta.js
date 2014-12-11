4d5b24694d835a29b7e478477571c1a635c41a1d
Timo Sallinen <timo.sallinen@nls.fi>
2014-12-11 09:33:14 +0200

    Added metadata script for cadastral index map.

--- nls-metadata/src/main/resources/kiinteistorekisterikartta.js
+++ nls-metadata/src/main/resources/kiinteistorekisterikartta.js
@@ -1,20 +1,21 @@
 /**
- * Kiinteistörekisterikartta
- * Metadatan käsittely 
- **/
+ *  * Kiinteistï¿½rekisterikartta
+ *   * Metadatan kï¿½sittely 
+ *    **/
 
-importClass(Packages.fi.nls.tiepalinfra.jcr.MetadataProperty);
+importClass(Packages.fi.nls.fileservice.jcr.MetadataProperty);
 
 handlerRegistry.registerDatasetHandler((function () {
 
 	var fileIdentifier = '472b3e52-5ba8-4967-8785-4fa13955b42e';
 	var datasetName = 'kiinteistorekisterikartta';
-	var pattern = /\/tuotteet\/kiinteistorekisterikartta\/(\S*)\/(\S*)\/(\S*)\/[0-9]*.zip/i; 
+	var pattern = /\/tuotteet\/kiinteistorekisterikartta\/kayttorajattu\/(\S*)\/(\S*)\/(\S*)\/\S*\/(\S*).zip/i; 
 				   
 	var mappers = new Array();
 	mappers.push(defaultMapper(MetadataProperty.NLS_DATASETVERSION));
 	mappers.push(lookupTableMapper(MetadataProperty.NLS_CRS, crsMappings));
 	mappers.push(lookupTableMapper(MetadataProperty.GMD_DISTRIBUTIONFORMAT, formatMappings));
+	mappers.push(defaultMapper(MetadataProperty.NLS_GRIDCELL));
 	
 	var handler = {
 			supportsPath: function (path) {
