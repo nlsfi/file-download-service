12dac4829faa6782a500bd0654f0ac748c67b0c5
Timo Sallinen <timo.sallinen@nls.fi>
2015-01-28 15:52:40 +0200

    Added support for new ModeShape privileges.

--- modeshape-authz/src/main/java/fi/nls/fileservice/security/Privilege.java
+++ modeshape-authz/src/main/java/fi/nls/fileservice/security/Privilege.java
@@ -6,16 +6,19 @@ public class Privilege {
     public static final Privilege ADD_NODE = new Privilege("add_node");
     public static final Privilege SET_PROPERTY = new Privilege("set_property");
     public static final Privilege REMOVE = new Privilege("remove");
-    public static final Privilege REMOVE_CHILD_NODES = new Privilege(
-            "remove_child_nodes");
-    public static final Privilege INDEX_WORKSPACE = new Privilege(
-            "index_workspace");
-
+    public static final Privilege REMOVE_CHILD_NODES = new Privilege("remove_child_nodes");
+    public static final Privilege INDEX_WORKSPACE = new Privilege("index_workspace");
+    public static final Privilege REGISTER_NAMESPACE = new Privilege("register_namespace");
+    public static final Privilege BACKUP = new Privilege("backup");
+    public static final Privilege RESTORE = new Privilege("restore");
+    public static final Privilege MONITOR = new Privilege("monitor");
+    
     private final String name;
 
     private static Privilege[] PRIVILEGES = new Privilege[] { Privilege.READ,
             Privilege.ADD_NODE, Privilege.SET_PROPERTY, Privilege.REMOVE,
-            Privilege.REMOVE_CHILD_NODES, Privilege.INDEX_WORKSPACE };
+            Privilege.REMOVE_CHILD_NODES, Privilege.INDEX_WORKSPACE, Privilege.REGISTER_NAMESPACE,
+            Privilege.BACKUP, Privilege.RESTORE, Privilege.MONITOR };
 
     private Privilege(String name) {
         this.name = name;
